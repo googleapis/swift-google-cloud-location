1566bcdc4f46d3b5a9658b30b4d459207049a807
Carlos O'Ryan <coryan@google.com>
2026-08-19 22:21:47 +0000

    chore: update librarian circa 2026-08-18 (#470)
    In this version of librarian we can automatically import some packages
    with a `@_spi()` attribute. That is handy to hide implementation details
    within `GoogleCloudWkt` and `GoogleCloudGax`.

--- Sources/GoogleCloudLocation/ListLocationsResponse.swift
+++ Sources/GoogleCloudLocation/ListLocationsResponse.swift
@@ -15,8 +15,8 @@
 // limitations under the License.
 
 import Foundation
-import GoogleCloudGax
-import GoogleCloudWkt
+@_spi(GoogleCloudInternal) import GoogleCloudGax
+@_spi(GoogleCloudInternal) import GoogleCloudWkt
 
 /// The response message for [Locations.ListLocations][google.cloud.location.Locations.ListLocations].
 ///
--- Sources/GoogleCloudLocation/Location.swift
+++ Sources/GoogleCloudLocation/Location.swift
@@ -15,7 +15,7 @@
 // limitations under the License.
 
 import Foundation
-import GoogleCloudWkt
+@_spi(GoogleCloudInternal) import GoogleCloudWkt
 
 /// A resource that represents Google Cloud Platform location.
 public struct Location: Codable, Equatable, GoogleCloudWkt._AnyPackable,
